392a7350c35d7a61f434935bd97483f8f9c416e6
饼yue <2310179201@qq.com>
2021-11-21 18:04:50 +0800

    Add files via upload

---
+++ src/components/icon/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <icon-font></icon-font>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component({
+  name: 'x-icon'
+})
+export default class extends Vue {
+  @Prop() private msg!: string
+}
+</script>
+
+<style scoped></style>
